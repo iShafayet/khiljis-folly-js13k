3ba634ea397dbd92602eb3e8380996c87960697a
Sayem Shafayet <ishafayet@gmail.com>
2023-09-09 17:11:10 +0600

    Add FPS Counter

---
+++ src/objects/FpsCounter.ts
@@ -0,0 +1,22 @@
+import { CANVAS_BASE_HEIGHT, CANVAS_BASE_WIDTH } from "../constants";
+
+export class FpsCounter {
+  lastTimeStamp = Date.now();
+  frameCount = 0;
+  fps = 0;
+
+  draw(ctx: CanvasRenderingContext2D) {
+    if (Date.now() - this.lastTimeStamp > 1000) {
+      this.fps = this.frameCount;
+      this.frameCount = 0;
+      this.lastTimeStamp = Date.now();
+    } else {
+      this.frameCount += 1;
+    }
+
+    let text = `${this.fps}`.padStart(2, "0");
+    ctx.font = "normal 24px Arial";
+    ctx.fillStyle = "yellow"
+    ctx.fillText(text, CANVAS_BASE_WIDTH - 40, 30);
+  }
+}
