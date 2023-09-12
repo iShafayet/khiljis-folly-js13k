6e5142bcc04d2d4fcaf461bb194cde45293fe52b
Sayem Shafayet <ishafayet@gmail.com>
2023-09-13 05:55:59 +0600

    Add advzip util to save more bytes!

--- scripts/zip-file.js
+++ scripts/zip-file.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const archiver = require("archiver");
+const { execSync } = require("child_process");
+const path = require("path");
 
 const distDir = process.cwd() + "/dist";
 const zippedDir = process.cwd() + "/zipped";
@@ -11,4 +13,18 @@ const archive = archiver("zip", { zlib: { level: 9 } });
 archive.pipe(output);
 archive.file(distDir + "/index.html", { name: "index.html" });
 
-archive.finalize();
+archive.finalize().then(() => {
+  const ADVANCED_COMP_DIR = `C:\\Dev\\advancedcomp`;
+  const COMMAND = `.\\advzip.exe -z -4 -i 1000 .\\game.zip`;
+
+  if (!fs.existsSync(ADVANCED_COMP_DIR)) {
+    console.log("advzip util is not available.");
+    return;
+  }
+  setTimeout(() => {
+    fs.renameSync("./zipped/game.zip", ADVANCED_COMP_DIR + "\\game.zip");
+    let res = execSync(COMMAND, { cwd: ADVANCED_COMP_DIR });
+    console.log(res.toString());
+    fs.renameSync(ADVANCED_COMP_DIR + "\\game.zip", "./zipped/game.zip");
+  }, 5000);
+});
